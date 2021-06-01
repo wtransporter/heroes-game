ea0d9a51e30df92a58c22e5c457c2e83d0f41337
vlada <vladapantic@hotmail.com>
2021-06-01 09:10:30 +0200

    Clean exception class

--- core/exceptions/WeaponNotAllowedException.php
+++ core/exceptions/WeaponNotAllowedException.php
@@ -2,9 +2,4 @@
 
 namespace app\core\exceptions;
 
-class WeaponNotAllowedException extends \Exception
-{
-    public function __construct($message, $code = 422, \Throwable $previous = null) {
-        parent::__construct($message, $code, $previous);
-    }
-}
+class WeaponNotAllowedException extends \Exception {}
